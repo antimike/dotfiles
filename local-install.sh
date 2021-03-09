f344ba82122ddd0cddc1bc87f9068b821cf31318
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-03-09 15:44:28 -0700

    Installed Tridactyl 'local' editor extension

--- local-install.sh
+++ local-install.sh
@@ -32,3 +32,4 @@ sudo apt-get install pcscd scdaemon gnupg2 pcsc-tools -y
 echo reader-port "Yubico Yubikey 4 OTP+U2F+CCID 00 00" > ~/.gnupg/scdaemon.conf
 sudo apt-get install i3xrocks-battery -y
 regolith-look refresh
+curl -fsSl https://raw.githubusercontent.com/tridactyl/native_messenger/master/installers/install.sh -o /tmp/trinativeinstall.sh && sh /tmp/trinativeinstall.sh 1.21.0
