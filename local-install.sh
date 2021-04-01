46b3d9e874b49c2f5e03dbe4a75138ef404e0ac1
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-04-01 00:53:10 -0600

    Installed LaTeX packages 'tabls' and 'xypic'

--- local-install.sh
+++ local-install.sh
@@ -3,3 +3,5 @@ pip3 install pandoc-eqnos --user
 sudo dnf install jq -y
 sudo dnf install 'tex(stackengine.sty)' -y
 pip3 install papis-scihub --user
+sudo dnf install 'tex(tabls.sty)' -y
+sudo dnf install 'tex(xypic.sty)' -y
