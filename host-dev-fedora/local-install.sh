973d97a73a6bae2694a9f0308201926c02f2d8df
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-04-12 19:51:12 -0600

    host-dev-fedora: Added history and local- files

---
+++ host-dev-fedora/local-install.sh
@@ -0,0 +1,12 @@
+# Haskell
+sudo dnf install cabal-install -y
+sudo dnf install hugs98 -y
+cabal update
+cabal install hscolour
+git clone https://github.com/rhysd/ghci-color ~/Source/ghci-color
+sudo ln -s ~/Source/ghci-color/ghci-color /usr/local/bin/ghci-color
+stack install stylish-haskell
+
+# NOTE: Doesn't work with either Stack or Cabal.  Investigate
+stack install hdevtools 
+*/5 8-20 * * 1-5 killall mbsync &>/dev/null; mbsync -a -V 2>&1 >> ~/.mbsync.log; /usr/bin/notmuch-hook.sh
